a54ad6937a0dc19e0bec6ae78cd61795ca61c65d
Kuba Bergel <kubabergel@gmail.com>
2020-01-22 10:39:19 +0100

    typescript

---
+++ src/components/Trolls/Trolls.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import NoData from "../NoData/NoData";
+
+const Trolls = () => (
+  <div className="trolls">
+    <NoData text="I'm sorry. There are no trolls at home now." />
+  </div>
+);
+
+export default Trolls;
